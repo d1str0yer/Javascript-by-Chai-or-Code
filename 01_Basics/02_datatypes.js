56f5db39cdb163f5023ff76b74218305e64175e8
d1str0yer <95298338+d1str0yer@users.noreply.github.com>
2023-12-15 09:28:54 +0000

    datatypes in Javascript - primitive and non-primitive

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // to tell to use newer version
+
+let emp_name = "Lalit";
+let age = 25;
+let has_loggedIn = false;
+let designation; // undefined
+
+// console.table([emp_name, age, has_loggedIn, designation]);
+/*
+Primitive datatypes:
+Number -> has a range of 2 to the power 53
+bigint -> to store a very big number
+string -> ""
+boolean -> true/false
+null -> standalone value
+undefined -> means no value assinged
+symbol -> unique
+
+Non-primitive datatype:
+object
+*/
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
