85c6c3364b16357373cc6a3066294122fc87228e
d1str0yer <95298338+d1str0yer@users.noreply.github.com>
2023-12-21 06:15:33 +0000

    Added more conversion examples

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -48,20 +48,59 @@ let c = "9";
 let d = 20;
 let e = null;
 let f = true;
-console.log(a+b+c+d+e+f); // 33ab0920nulltrue
+// console.log(a+b+c+d+e+f); // 33ab0920nulltrue
 // console.log(b+c); // 09 
 // console.log(d+c+b); // 2090
 // console.log(d+b+c); // 209
-console.log(typeof(d+e)); // number
-console.log(typeof(e+d)); // number
-console.log(e+d); // 20
-console.log(e+b); // 0
-console.log(e+f); // 1
-console.log(f+e); // 1
-console.log(typeof(e+f)); // number
-console.log(typeof(f+e)); // number
-console.log(d+e+f); // 21
-console.log(typeof(d+e+f)); // number
+// console.log(typeof(d+e)); // number
+// console.log(typeof(e+d)); // number
+// console.log(e+d); // 20
+// console.log(e+b); // 0
+// console.log(e+f); // 1
+// console.log(f+e); // 1
+// console.log(typeof(e+f)); // number
+// console.log(typeof(f+e)); // number
+// console.log(d+e+f); // 21
+// console.log(typeof(d+e+f)); // number
+
+// console.log("1" + 1 ); // 11
+// console.log( 1 + "1" ); // 11
+// console.log( 1 + 1 + "1" ); // 21
+// console.log( "1" + 1 + 1 ); // 111
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+// console.log( (3 + 4) * 5 % 3); // 7 * 5 % 3 = 35 % 3 = 2
+// console.log(+true); // 1
+// console.log(+""); // 0
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2 + 2 // bad practice - avoid this
+// console.log(num1); // 4
+// console.log(num2); // 4
+// console.log(num3); // 4
+
+// let gameCounter = 100
+// gameCounter++; // 100
+// ++gameCounter;  // 102
+// console.log(gameCounter); // 102
+
+let x = 10;
+let y = 20;
+let z = x++ + ++y + x++ + y++;
+//  z = 10 + 21 + 11 + 21 = 63
+let r = ++y + x++ + z++ + y++;
+//  r = 23 + 12 + 63 + 23 = 121
+console.log(x); // 13
+console.log(y); // 24
+console.log(z); // 64
+console.log(r); // 121
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
 
 
 
